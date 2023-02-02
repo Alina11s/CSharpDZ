a12e702153edba00d45d75cd223c2ca519eba2f7
Alina <alina.sablina2016@yandex.ru>
2023-02-02 19:41:01 +0300

    Решены задачи 25, 27, 29

---
+++ task25/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+и возводит число A в натуральную степень B.*/
+
+Console.Clear();
+int getUserNumber(string message)
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+int A = getUserNumber("Введите число A: ");
+int end = getUserNumber("Введите число B: ");
+
+double getPow(int start, int end)
+{
+    double pr = 1;
+    for (int B = start; B <= end; B++ )
+    {
+        pr = pr * A;
+    }
+    return pr;
+}
+
+double pr = getPow(1,end);
+Console.WriteLine($"Число {A} в степени {end} равно {pr}");
+
---
+++ task27/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.*/
+
+Console.Clear();
+int getUserNumber(string message)
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int getSumOfnumber (int number)
+{
+    int sum = 0;
+    int A = 0;
+    while (number > 0)
+    {
+        A = number % 10;
+        number = number / 10;
+        sum = sum + A;
+    }
+    return sum;
+}
+
+int number = getUserNumber("Введите число: ");
+int sum = getSumOfnumber(number);
+Console.WriteLine($"Сумма цифр в числе {number} равна {sum}");
