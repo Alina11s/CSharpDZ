2bab56e8883da7eb115eae9e18b16fbef2500857
Alina <a.sablina011@gmail.com>
2023-02-20 18:28:40 +0300

    Решены задачи 64 и 66

---
+++ task64/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая 
+выведет все натуральные числа в промежутке от N до 1.*/
+
+Console.Clear();
+int getFromUserData(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+int userNumber = getFromUserData("Введите N: ");
+printRange(userNumber);
+
+void printRange(int N)
+{
+    if (N == 0)
+    {
+        return;
+    }
+    Console.Write(N + ",");
+    printRange(N - 1);
+}
---
+++ task66/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.*/
+
+Console.Clear();
+int getFromUserData(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+
+int userNumberStart = getFromUserData("Введите M: ");
+int userNumberFinish = getFromUserData("Введите N: ");
+
+int getSumOfNumber(int userNumberStart, int userNumberFinish)
+{
+    if (userNumberStart == userNumberFinish)
+    {
+        return userNumberStart;
+    }
+    return userNumberStart + getSumOfNumber(userNumberStart + 1, userNumberFinish);
+}
+
+int result = getSumOfNumber(userNumberStart, userNumberFinish);
+Console.WriteLine(result);
+
